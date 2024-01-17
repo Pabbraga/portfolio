bab22952e24ad121b48033a6f1770fdee8f8b536
Pabbraga <pablofelix567@gmail.com>
2024-01-17 19:30:16 -0300

    feat: add language switch script and some changes to fit it

---
+++ js/translate.js
@@ -0,0 +1,38 @@
+import pt from '../lang/pt.json' assert {type:'json'};
+import en from '../lang/en.json' assert {type:'json'};
+
+let actualLang = localStorage.getItem("lang")?localStorage.getItem("lang").toLowerCase():localStorage.setItem("lang", "PT");
+
+function changeText(lang) {
+    for(const item in lang) {
+        if(item === "projects") {
+            for(const project in lang.projects) {
+                document.getElementById(project.replace('_', '-')).innerText = lang.projects[project];
+            }
+            return;
+        }
+        document.getElementById(item.replace('_', '-')).innerText = lang[item];
+    }
+}
+
+function handleText() {
+    switch (actualLang) {
+        case 'pt':
+            changeText(pt);
+            break;
+        case 'en':
+            changeText(en);
+            break;
+    }
+}
+
+function translate (e) {
+    const inputLang = e.target.innerText;
+    localStorage.setItem("lang", inputLang);
+    actualLang = localStorage.getItem("lang").toLowerCase();
+    handleText();
+}
+
+document.addEventListener("DOMContentLoaded", handleText);
+
+export default translate;
